42b4ab8818b53b5426fd1c249179b83c4d43d1a7
josehenriquebr <josehenriquebr@gmail.com>
2020-03-29 01:23:47 -0300

    finalizando o backend

--- semana11/aulas/backend/src/routes.js
+++ semana11/aulas/backend/src/routes.js
@@ -27,13 +27,18 @@
 const express = require("express");
 const ongController = require("./controllers/ongController");
 const incidentsController = require("./controllers/incidentsController");
+const profileController = require("./controllers/profileController");
+const sessionControler = require("./controllers/sessionController");
 
 const routes = express.Router();
+
+routes.post("/sessions", sessionControler.create);
 
 routes.get("/ongs", ongController.index);
 routes.post("/ongs", ongController.create);
 
 routes.get("/incidents", incidentsController.index);
+routes.get("/profile", profileController.index);
 routes.post("/incidents", incidentsController.create);
 routes.delete("/incidents/:id", incidentsController.delete);
 
